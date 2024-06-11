e91755947ed299ff4771fa12edf4c994ae9897e4
Andrew Eardley <eardley.andrew1@gmail.com>
2024-06-11 12:51:02 -0600

    updated reference

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ Vue.createApp({
             addresses: [],
             nameInput: "",
             addressInput: "",
-            
+
 
         };
     },
     methods: {
-
-
+        validateInputs() {
+            return (this.nameInput.length > 0 && this.addressInput.length > 0);
+        },
+        submit() {
+            addresses.push({name: this.nameInput, address: this.addressInput})
+            this.nameInput = "";
+            this.addressInput = "";
+        }
     },
     created: function() {
 
